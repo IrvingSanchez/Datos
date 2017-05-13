22a1b9152ab64d6c75c517011022abfbc0a62feb
irving <irving.a.sanchez88@gmail.com>
2017-05-12 19:30:05 -0500

    Shared Preferences y Archivos

--- app/src/main/java/com/example/pc_8/datos/MainActivity.java
+++ app/src/main/java/com/example/pc_8/datos/MainActivity.java
@@ -3,7 +3,14 @@ package com.example.pc_8.datos;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +25,48 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences preferencias;
     SharedPreferences.Editor editor;
 
+    OutputStreamWriter archivoEscritura;
+    TextView encabezado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        encabezado = (TextView) findViewById(R.id.encabezado);
 
+        /**
+         * Guarda Datos en Preferencias Compartidas
+         */
         //  Nombre del archivo y modo de privacidad
         preferencias = getSharedPreferences("Preferencias",MODE_PRIVATE);
         editor = preferencias.edit();
+        //  Agrega datos a SP
+        agregaDatosSP();
 
+        //  Muestra los datos
+        recuperaDatosSP();
+
+        /**
+         * Guarda Datos en archivos
+         */
+        try {
+            archivoEscritura = new OutputStreamWriter(
+                    openFileOutput("archivoInterno.txt", MODE_PRIVATE));
+            archivoEscritura.write("Estoy escribiendo en el archivo");
+            archivoEscritura.write("\n Estoy escribiendo de nuevo.");
+            archivoEscritura.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        leerDatosArchivo();
+
+
+    }
+
+    private void agregaDatosSP()
+    {
         //  Agregar valores al archivo
         editor.putString("player1","Mario Bross");
         editor.putInt("vidas",3);
@@ -37,11 +77,8 @@ public class MainActivity extends AppCompatActivity {
 
         //  Aplicar cambios
         editor.commit();
-        //  Muestra los datos
-        recuperaDatos();
     }
-
-    private void recuperaDatos()
+    private void recuperaDatosSP()
     {
         String player1 = preferencias.getString("player1","");
         int vidas = preferencias.getInt("vidas",0);
@@ -57,4 +94,23 @@ public class MainActivity extends AppCompatActivity {
                                 "\nActivo: " + activo,
                 Toast.LENGTH_SHORT).show();
     }
+
+    private void leerDatosArchivo()
+    {
+        try {
+            BufferedReader archivoLectura = new BufferedReader(new InputStreamReader(
+                    openFileInput("archivoInterno.txt")));
+            String linea;
+            encabezado.setText("Esto dice el archivo: ");
+            while ( (linea = archivoLectura.readLine()) != null)
+            {
+                encabezado.append("\n" + linea);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
